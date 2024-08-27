d23aa82910e9f0eb316112e28d594d873e8adf84
crschlosser <carlos.schlosser@yahoo.com.br>
2024-08-27 19:17:57 -0300

    EM 27/08

---
+++ Models/ContaCorrente.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploPOO.Models
+{
+    public class ContaCorrente
+    {
+        public ContaCorrente(int numeroConta,decimal saldoInicial)
+        {
+            NumeroConta=numeroConta;
+            saldo = saldoInicial;
+        }
+        public int NumeroConta { get; set; }
+        private decimal saldo;
+        private int v1;
+        private double v2;
+
+        public void Sacar(Decimal valor)
+        {
+            if (valor>saldo)
+            {
+                Console.WriteLine("Saldo Insificiente");
+            }
+            else
+            {
+            saldo -= valor;
+            Console.WriteLine("Saldo é : "+saldo+" saque");
+            }
+        }
+        public void Depositar(Decimal valor)
+        {
+            saldo += valor;
+            Console.WriteLine("saldo é : "+saldo+" deposito");
+        }
+        public void ExibirSaldo()
+        {
+            Console.WriteLine("saldo : " +saldo);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using ExemploPOO.Models;
+
+Console.Clear();
+
+
+ContaCorrente c1 = new ContaCorrente(123,1554);
+c1.ExibirSaldo();
+c1.Depositar(100);
+c1.Depositar(200);
+c1.Sacar(100);
+c1.Sacar(100000);
+c1.Sacar(100);
+
+
+
+
+
+
+
+// Pessoa p1 =new Pessoa();
+
+// p1.Nome ="Carlos";
+// p1.Idade = 60;
+
+// p1.Apresentar();
